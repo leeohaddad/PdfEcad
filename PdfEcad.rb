00d5d460b85bb35ddc3b55b2ece32cd7c7286b98
Leonardo Haddad <leo.haddad1@gmail.com>
2016-07-24 14:31:20 -0300

    Parse specific informations about work (without right_holders).

--- PdfEcad.rb
+++ PdfEcad.rb
@@ -9,7 +9,12 @@ module Importers
           end
           
           def works
+               $counter = 0
+               hashes = []
                reader = PDF::Reader.new(@pdfPath)
+               state = 0 # "Seeking work"
+               workLine = ""
+               # iterate over pages
                reader.pages.each do |page|
                     pageLines = page.text.split("\n")
                     # pageLines[0]:  RELATÓRIO ANALÍTICO DE TITULAR AUTORAL E SUAS OBRAS
@@ -28,16 +33,55 @@ module Importers
                     # pageLines[13]: 
                     # pageLines[14]: 
                     loop_pivot = 15
-                    while loop_pivot < pageLines.count-1 do
-                         if pageLines[loop_pivot].strip.length > 0
-                              work(pageLines[loop_pivot])
+                    # iterate over lines of each page
+                    while loop_pivot < pageLines.length-1 do
+                         # catch only valid lines
+                         if pageLines[loop_pivot].length > 0 && numeric?(pageLines[loop_pivot][0]) != nil then
+                              if (state == 0) then # new work
+                                   workLine = pageLines[loop_pivot]
+                                   loop_pivot += 1
+                                   state = 1 # "Adding right holders"
+                              else
+                                   if (pageLines[loop_pivot][pageLines[loop_pivot].length-5] == '/') then # last work finished at end of page
+                                        state = 2 # "Preparing to call work()"
+                                        hashes.push(work(workLine))
+                                        state = 0 # "Seeking work"
+                                   else # the work is present/described in both pages
+                                        workLine += pageLines[loop_pivot]
+                                        loop_pivot += 1
+                                        state = 1 # "Adding right holders"
+                                   end
+                              end
+                              while state == 1 do # keep registering right holders
+                                   while (pageLines[loop_pivot].length == 0) do # skip empty lines
+                                        loop_pivot += 1
+                                   end
+                                   if numeric?(pageLines[loop_pivot][0]) != nil && numeric?(pageLines[loop_pivot][pageLines[loop_pivot].length-1]) != nil && pageLines[loop_pivot][pageLines[loop_pivot].length-5]!='/' then
+                                        # register a right holder
+                                        workLine += ' | ' + pageLines[loop_pivot]
+                                        if pageLines[loop_pivot].length < 8 then # fix for specific error
+                                             loop_pivot += 1
+                                             workLine += pageLines[loop_pivot]
+                                        end
+                                        loop_pivot += 1
+                                   else
+                                        if (loop_pivot < pageLines.length-1)
+                                             state = 2 # "Preparing to call work()"
+                                             hashes.push(work(workLine))
+                                             state = 0 # "Seeking work"
+                                        else
+                                             state = 3 # "Work transcending pages"
+                                             workLine += ' | '
+                                        end
+                                   end
+                              end
+                         else
+                              loop_pivot += 1
                          end
-                         loop_pivot += 1
                     end
                     # pageLines[count-1]: 04/03/2015 - 16:35 *Situação da Obra : LB-LIBERADO / BL-BLOQUEADA / DU-DUPLICIDADE / HO-HOMONIMA / DP-DOMINIO PUBLICO / DE-DERIVADA / COPágina 4A / EC-EM CONFLITO
-                    puts ' '
-                    puts ' '
                end
+               puts "total: #{$counter}"
           end
           
           def right_holder(line)
@@ -45,7 +89,59 @@ module Importers
           end
           
           def work(line)
-               puts "work line:|#{line}|"
+               $counter += 1
+               components = line.split(' | ')
+               # step 1: parse informations about the work
+               workDs = components[0]
+               informations = workDs.split('   ')
+               workHash = Hash.new
+               index = 0
+               # get external_id
+               while informations[index].length == 0 do
+                    index += 1
+               end
+               external_ids = []
+               external_id = Hash.new
+               external_id["source_name"] = "Ecad"
+               external_id["source_id"] = informations[index].strip
+               external_ids.push(external_id)
+               workHash["external_ids"] = external_ids
+               index += 1
+               # get iswc
+               while informations[index].length == 0 do
+                    index += 1
+               end
+               workHash["iswc"] = informations[index].strip
+               index += 1
+               while informations[index].length == 0 do
+                    index += 1
+               end
+               # if there is no iswc for this work, iswc is "- . . -"
+               while  informations[index] == '.' || informations[index] == '-' do
+                    workHash["iswc"] += ' ' + informations[index].strip
+                    index += 1
+               end
+               # get title
+               while informations[index].length == 0 do
+                    index += 1
+               end
+               workHash["title"] = informations[index].strip
+               # get situation
+               index += 1
+               while informations[index].length == 0 do
+                    index += 1
+               end
+               workHash["situation"] = informations[index].strip
+               index += 1
+               # get created_at
+               while informations[index].length == 0 do
+                    index += 1
+               end
+               workHash["created_at"] = informations[index].strip
+          end
+
+          def numeric?(lookAhead)
+               lookAhead =~ /[[:digit:]]/
           end
 
      end
